d4bdd1964e2c418c40bd1acb017fd23f6333505b
unknown <ludmila.lopez@elaniin.com>
2023-01-18 18:24:06 -0300

    fix :pencil2: : changed some erroneous or outdated data about the database

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'foodiedb'),
+      host: env('DATABASE_HOST', 'containers-us-west-127.railway.app'),
+      port: env.int('DATABASE_PORT', 7876),
+      database: env('DATABASE_NAME', 'railway'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'ginozega'),
+      password: env('DATABASE_PASSWORD', 'PGMl5pgjp6INsFiN4lw1'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
